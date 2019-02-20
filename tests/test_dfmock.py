e67230268745714ab10dfe4b33547f8fb2aaa3e2
Ethan Knox <emk@integrichain.com>
2019-02-20 09:58:42 -0500

    tests for tz

--- tests/test_dfmock.py
+++ tests/test_dfmock.py
@@ -1,6 +1,7 @@
 import pytest
 import pandas as pd
 from dfmock.dfmock import DFMock
+from datetime import datetime
 
 def test_mock_datetime_simple():
     mock = DFMock()
@@ -9,6 +10,17 @@ def test_mock_datetime_simple():
     
     assert len(stamps) == 1
     assert isinstance(stamps[0], pd.Timestamp)
+
+def test_mock_datetime_past():
+    mock = DFMock()
+    stamps = mock._mock_datetime(count=100, allow_future=False)
+    in_future = False
+    for stamp in stamps:
+        if stamp > pd.Timestamp(ts_input = datetime.now().isoformat() + " +4"):
+            in_future = True
+    assert not in_future 
+
+
 
 def test_mock_string():
     pass
